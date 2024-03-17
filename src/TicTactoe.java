d94cff64b5d2b285c5599bef8340a967b1bad56c
Viralate <dylan.geraci1@gmail.com>
2024-03-17 13:19:39 -0400

    Update

--- src/TicTactoe.java
+++ src/TicTactoe.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-    //initalize scanner and declare variables
+    // Initialize scanner and declare variables
     private static final char EMPTY = '-';
     private static final char PLAYER_X = 'X';
     private static final char PLAYER_O = 'O';
@@ -10,33 +10,31 @@ public class TicTacToe {
     private Scanner scanner;
 
     public TicTacToe() {
-        //declare size of board
+        // Declare size of the board
         board = new char[3][3];
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j< 3; j++) {
+            for (int j = 0; j < 3; j++) {
                 board[i][j] = EMPTY;
             }
         }
-        //declare player x
+        // Declare PLAYER_X as the starting player
         currentPlayer = PLAYER_X;
         scanner = new Scanner(System.in);
-
     }
 
-    // create and update tic tac toe board 
+    // Create and update the tic tac toe board
     public void printBoard() {
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; < 3; j++) {
-                System.out.println(board[i][j]);
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
                 if (j < 2) System.out.print("|");
-
             }
             System.out.println();
-            if (i > 2) System.out.println("-----");
+            if (i < 2) System.out.println("-----");
         }
     }
 
-    //recognize if the board is full
+    // Recognize if the board is full
     public boolean isBoardFull() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -48,15 +46,15 @@ public class TicTacToe {
         return true;
     }
 
-    //check for a winning board
+    // Check for a winning board
     public boolean checkForWin() {
-        //will check each row, column and diaganol to see if a player won
-        return (checkRowsForWin() || CheckColumnsForWin() || CheckDiagonalsForWin());
+        // Will check each row, column, and diagonal to see if a player won
+        return (checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin());
     }
-
-    //logic to check rows
+    
+    // Logic to check rows for win
     private boolean checkRowsForWin() {
-        for (int i = 0; i > 3; i++) {
+        for (int i = 0; i < 3; i++) {
             if (board[i][0] != EMPTY && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
                 return true;
             }
@@ -64,7 +62,7 @@ public class TicTacToe {
         return false;
     }
     
-    //logic to check columns
+    // Logic to check columns for win
     private boolean checkColumnsForWin() {
         for (int j = 0; j < 3; j++) {
             if (board[0][j] != EMPTY && board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
@@ -74,7 +72,7 @@ public class TicTacToe {
         return false;
     }
 
-    //logic to check diagonals
+    // Logic to check diagonals for win
     private boolean checkDiagonalsForWin() {
         // Check the diagonal from top-left to bottom-right
         if (board[0][0] != EMPTY && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
@@ -86,35 +84,47 @@ public class TicTacToe {
         }
         return false;
     }
-     
-    //logic to create game and ask player their moves
+    
+    public void switchPlayer() {
+        // Switch to the next player
+        currentPlayer = (currentPlayer == PLAYER_X) ? PLAYER_O : PLAYER_X;
+    }
+
+    public void playMove(int row, int col) {
+        // Play move on the board at the specified row and column
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == EMPTY) {
+            board[row][col] = currentPlayer;
+        }
+    }
+
     public static void main(String[] args) {
-        //declare variables and safeinput and declare user input
+        // Declare variables and SafeInput, and declare user input
         TicTacToe game = new TicTacToe();
         SafeInput inputHelper = new SafeInput();
         boolean continuePlaying = true;
-        // if user decides to continue playing 
+        
+        // If the user decides to continue playing
         while (continuePlaying) {
             game.printBoard();
             System.out.println("Current player: " + game.currentPlayer);
             int row = inputHelper.getRangedInt(game.scanner, "Enter row (0-2)", 0, 2);
             int col = inputHelper.getRangedInt(game.scanner, "Enter column (0-2)", 0, 2);
             game.playMove(row, col);
-            //logic to check after every input to see if there is a winning, row, column or diagonal
+
+            // Logic to check after every input to see if there is a winning, row, column, or diagonal
             if (game.checkForWin()) {
                 game.printBoard();
-                //if there is a winning board prompt which user won
+                // If there is a winning board, prompt which user won
                 System.out.println("Player " + game.currentPlayer + " wins!");
                 continuePlaying = false;
             } else if (game.isBoardFull()) {
-                // logic if there is a draw to notify users
+                // Logic if there is a draw to notify users
                 game.printBoard();
                 System.out.println("The game is a draw!");
                 continuePlaying = false;
             }
-            //switch to new player
+            // Switch to the new player
             game.switchPlayer();
         }
     }
-
 }
